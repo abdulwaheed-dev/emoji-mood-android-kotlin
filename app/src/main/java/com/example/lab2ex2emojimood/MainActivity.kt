7c448389ac04bc194deeea2ebb1c12ef0c36e00b
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-25 10:27:31 +0500

    functionality implemented

--- app/src/main/java/com/example/lab2ex2emojimood/MainActivity.kt
+++ app/src/main/java/com/example/lab2ex2emojimood/MainActivity.kt
@@ -1,11 +1,25 @@
-fpackage com.example.lab2ex2emojimood
+package com.example.lab2ex2emojimood
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var emoji = findViewById<ImageView>(R.id.emoji)
+        var msg = findViewById<TextView>(R.id.message)
+        var btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener(){
+            emoji.setImageResource(R.drawable.happy)
+            msg.text = "I'm so full :)"
+            btn.text = "Done"
+        }
     }
 }
