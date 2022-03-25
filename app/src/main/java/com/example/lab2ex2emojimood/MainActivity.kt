d47ad3b74df033b46e067ed533c070f776fa72e8
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-03-25 09:58:18 +0500

    init commit

---
+++ app/src/main/java/com/example/lab2ex2emojimood/MainActivity.kt
@@ -0,0 +1,11 @@
+fpackage com.example.lab2ex2emojimood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
